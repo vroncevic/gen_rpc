31935c04d406173e3f97f893ace26c9f4cb5318d
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-11-11 18:24:05 +0100

    [gen_rpc] updated setup for metaclasses, docs, github workflow actions

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = 'Copyright 2019, https://vroncevic.github.io/gen_rpc'
 __credits__ = ['Vladimir Roncevic']
 __license__ = 'https://github.com/vroncevic/gen_rpc/blob/dev/LICENSE'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -102,7 +102,7 @@ APPROVED_LICENSES = [
 PYP_CLASSIFIERS = SUPPORTED_PY_VERSIONS + APPROVED_LICENSES
 setup(
     name='gen_rpc',
-    version='1.0.0',
+    version='1.0.1',
     description='Generating RPC modules',
     author='Vladimir Roncevic',
     author_email='elektron.ronca@gmail.com',
